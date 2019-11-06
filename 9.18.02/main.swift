3f3feb51d8edfab6b0da173193bd1f37c29de21b
20181105879 <2605753145@qq.com>
2019-11-06 10:33:56 +0800

    ew
    rwee

--- 9.18.02/main.swift
+++ 9.18.02/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  9.18.02
+//  9.18.01
 //
 //  Created by s20181105879 on 2019/9/18.
 //  Copyright © 2019 s20181105879. All rights reserved.
@@ -8,5 +8,17 @@
 
 import Foundation
 
-print("Hello, World!")
+var i:Int=0;
+var sum=0;
+
+while i<=100
+{
+    if(i%7==0)
+    {
+        continue;
+    }
+    sum+=i;
+    i=i+1;
+}
+print(sum)
 
